89b16b031d67f074e5ca4893a27ad1e8365db3e3
SALMAN <SALMAN@DESKTOP-GRFBRBV>
2024-12-30 21:32:18 +0500

    Add project files.

---
+++ online doctor appoinment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace online_doctor_appoinment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
